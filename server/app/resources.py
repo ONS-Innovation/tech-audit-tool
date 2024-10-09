96852e624476fb510d86084976f56c5f0740b3d8
Sebastian <Sebastian.Evison-Theodossiades@ons.gov.uk>
2024-10-09 11:18:56 +0100

    More response code and documented for responses

--- server/app/resources.py
+++ server/app/resources.py
@@ -25,6 +25,7 @@ def require_owner_email(func):
 @ns.route("/projects")
 @ns.doc(params=required_param)
 class Projects(Resource):
+    @ns.doc(responses={200: 'Success', 400: 'owner_email is required'})
     @ns.marshal_with(get_project_model(), as_list=True)
     @require_owner_email
     def get(self):
@@ -32,18 +33,19 @@ class Projects(Resource):
         owner_email = args['owner_email']
         data = read_data()
         user_projects = [proj for proj in data['projects'] if proj["user"][0]['email'] == owner_email]
-        return user_projects
+        return user_projects, 200
     
     @ns.expect(get_project_model())
+    @ns.doc(responses={201: 'Created project', 400: 'owner_email is required', 406: 'Missing JSON data', 409: 'Project with the same name and owner already exists'})
     @require_owner_email
     def post(self):
         new_project = ns.payload
         if 'user' not in new_project or 'details' not in new_project or 'email' not in new_project['user'][0] or 'name' not in new_project['details']:
-            abort(400, description="owner_email and name are required")
+            abort(406, description="Missing JSON data")
         
         data = read_data()
         if any(proj['details']['name'] == new_project['details']['name'] and proj['user'][0]['email'] == new_project['user'][0]['email'] for proj in data['projects']):
-            abort(400, description="Project with the same name and owner already exists")
+            abort(409, description="Project with the same name and owner already exists")
         
         data['projects'].append(new_project)
         write_data(data)
@@ -68,7 +70,8 @@ class Projects(Resource):
         return new_project, 201
 
 
-@ns.doc(params=required_param)
+@ns.doc(params=required_param,
+responses={200: 'Success', 400: 'owner_email is required', 404: 'Project not found.'})
 @ns.route("/projects/<string:project_name>")
 class ProjectDetail(Resource):
     @ns.marshal_list_with(get_project_model())
@@ -80,7 +83,7 @@ class ProjectDetail(Resource):
         project = next((proj for proj in data['projects'] if proj["details"]['name'] == project_name and proj["user"][0]['email'] == owner_email), None)
         if not project:
             abort(404, description="Project not found")
-        return project
+        return project, 200
 
 
 
@@ -89,7 +92,8 @@ autoCompleteParser = reqparse.RequestParser()
 autoCompleteParser.add_argument('type', type=str, required=True, help='type is required')
 autoCompleteParser.add_argument('search', type=str, required=True, help='search is required')
 
-@ns.doc(params={'type':'Type of array', 'search':'Search query'})
+@ns.doc(params={'type':'Type of array', 'search':'Search query'}, 
+responses={200: 'Success', 400: 'type and search are required', 404: 'No matches found.', 406: 'Invalid type', 411: 'Search query is too long'})
 @ns.route("/autocomplete")
 class Autocomplete(Resource):
     def get(self):
@@ -101,16 +105,19 @@ class Autocomplete(Resource):
             abort(400, description="type and search are required")
         
         if search_type not in ['languages', 'IDEs', 'misc']:
-            abort(400, description="Invalid type")
+            abort(406, description="Invalid type")
         
         if len(search_query) > 16:
-            abort(400, description="Search query is too long")
+            abort(411, description="Search query is too long")
         
         array_data = read_array_data()
         
         if search_type not in array_data:
-            abort(400, description=f"Invalid type: {search_type}")
+            abort(406, description=f"Invalid type: {search_type}")
         
         results = [item for item in array_data[search_type] if search_query.lower() in item.lower()]
+
+        if not results:
+            abort(404, description="No matches found")
         
-        return results
+        return results, 200
